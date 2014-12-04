bda5ae7e16f510771c8da68010b1b9192bda2b9e
Aden <aden@jacobbailey.com>
2014-12-04 19:29:46 +0000

    master : Create Menus Table Migration

---
+++ src/migrations/2014_12_01_184143_create_menus_tables.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('menus', function ($table) {
+
+			$table->increments('id');
+			$table->string('name');
+			$table->string('identifier')->unique();
+
+			$table->timestamps();
+
+		});
+
+		Schema::create('menuitems', function ($table) {
+
+			$table->increments('id');
+			$table->integer('menu_id');
+			$table->string('name');
+			$table->integer('page_id');
+			$table->string('link');
+			$table->integer('order');
+
+			$table->timestamps();
+			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('menus');
+		Schema::drop('menuitems');
+	}
+
+}
