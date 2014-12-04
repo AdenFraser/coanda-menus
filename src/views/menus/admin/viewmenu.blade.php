6973372b294dafbfbcd0b9f4124e1adbe14cce6f
Aden <aden@jacobbailey.com>
2014-12-04 20:08:07 +0000

    master : Ability to drag and drop menu items now functions. Styling and save via JSON serialized array of data is required next.

--- src/views/menus/admin/viewmenu.blade.php
+++ src/views/menus/admin/viewmenu.blade.php
@@ -70,26 +70,21 @@
 					{{ Form::open(['url' => Coanda::adminUrl('menus/view-menu/' . $menu->id) ]) }}
 
 						@if ($menus->count() > 0)
-							<table class="table table-striped">
-								<thead>
-									<tr>
-										<th class="tight"></th>
-										<th>Name</th>
-										<th class="tight"></th>
-										<th class="tight"></th>
-										<th class="tight"></th>
-									</tr>
-								</thead>
+							<ol class='nested_with_switch sortable-menu vertical'>
 								@foreach ($menus as $menu)
-									<tr>
-										<td><input type="checkbox" name="remove_menu_ids[]" value="{{ $menu->id }}"></td>
-										<td><a href="{{ Coanda::adminUrl('menus/view/' . $menu->id) }}">{{ $menu->name }}</a></td>
-										<td>{{ Form::text('ordering[' . $menu->id . ']', $menu->order, ['class' => 'form-control input-sm', 'style' => 'width: 50px;']) }}</td>
-										<td><a href="{{ Coanda::adminUrl('menus/edit/' . $menu->id) }}"><i class="fa fa-pencil-square-o"></i></a></td>
-										<td><a href="{{ Coanda::adminUrl('menus/remove/' . $menu->id) }}"><i class="fa fa-minus-circle"></i></a></td>
-									</tr>
+									<li>
+										<i class="fa fa-arrows"></i>
+										<input type="checkbox" name="remove_menu_ids[]" value="{{ $menu->id }}">
+										<a href="{{ Coanda::adminUrl('menus/view/' . $menu->id) }}">{{ $menu->name }}</a>
+										{{ Form::text('ordering[' . $menu->id . ']', $menu->order, ['class' => 'form-control input-sm', 'style' => 'width: 50px;']) }}
+										<a href="{{ Coanda::adminUrl('menus/edit/' . $menu->id) }}"><i class="fa fa-pencil-square-o"></i></a>
+										<a href="{{ Coanda::adminUrl('menus/remove/' . $menu->id) }}"><i class="fa fa-minus-circle"></i></a>
+										<ol>
+
+										</ol>
+									</li>
 								@endforeach
-							</table>
+							</ol>
 
 							{{ $menus->links() }}
 						@else
@@ -98,7 +93,7 @@
 
 						<div class="buttons">
 							{{ Form::button('Remove selected', ['name' => 'remove_selected', 'value' => 'true', 'type' => 'submit', 'class' => 'btn btn-default']) }}
-							{{ Form::button('Update orders', ['name' => 'update_order', 'value' => 'true', 'type' => 'submit', 'class' => 'btn btn-default pull-right']) }}
+							{{ Form::button('Save menu order', ['name' => 'update_order', 'value' => 'true', 'type' => 'submit', 'class' => 'btn btn-default pull-right']) }}
 						</div>
 
 					{{ Form::close() }}
@@ -110,8 +105,133 @@
 </div>
 
 @section('custom-js')
-	<script src="{{ asset('packages/adenfraser/coanda-menus/js/jquery.sortable.js') }}"></script>
+	<script src="{{ asset('packages/adenfraser/coanda-menus/js/jquery.sortable.min.js') }}"></script>
 	<script src="{{ asset('packages/adenfraser/coanda-menus/js/coanda-menus-scripts.js') }}"></script>
+
+	<style>
+	body.dragging, body.dragging *
+	{
+		cursor: move !important;
+	}
+	.dragged {
+    opacity: 0.5;
+    position: absolute;
+    top: 0;
+    z-index: 2000;
+}
+ol.vertical {
+    margin: 0 0 9px;
+    min-height: 10px;
+}
+ol.vertical li {
+    background: none repeat scroll 0 0 #eeeeee;
+    border: 1px solid #cccccc;
+    color: #0088cc;
+    display: block;
+    margin: 5px;
+    padding: 5px;
+}
+ol.vertical li.placeholder {
+    border: medium none;
+    margin: 0;
+    padding: 0;
+    position: relative;
+}
+ol.vertical li.placeholder:before {
+    -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    border-color: transparent -moz-use-text-color transparent red;
+    border-image: none;
+    border-style: solid none solid solid;
+    border-width: 5px medium 5px 5px;
+    content: "";
+    height: 0;
+    left: -5px;
+    margin-top: -5px;
+    position: absolute;
+    top: -4px;
+    width: 0;
+}
+ol {
+    list-style-type: none;
+}
+ol i.icon-move {
+    cursor: pointer;
+}
+ol li.highlight {
+    background: none repeat scroll 0 0 #333333;
+    color: #999999;
+}
+ol li.highlight i.icon-move {
+    background-image: url("../img/glyphicons-halflings-white.png");
+}
+ol.nested_with_switch, ol.nested_with_switch ol {
+    border: 1px solid #eeeeee;
+}
+ol.nested_with_switch.active, ol.nested_with_switch ol.active {
+    border: 1px solid #333333;
+}
+ol.nested_with_switch li, ol.simple_with_animation li, ol.serialization li, ol.default li {
+    cursor: pointer;
+}
+ol.simple_with_animation {
+    border: 1px solid #999999;
+}
+.switch-container {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 80px;
+}
+.navbar-sort-container {
+    height: 200px;
+}
+ol.nav li, ol.nav li a {
+    cursor: pointer;
+}
+ol.nav .divider-vertical {
+    cursor: default;
+}
+ol.nav li.dragged {
+    background-color: #2c2c2c;
+}
+ol.nav li.placeholder {
+    position: relative;
+}
+ol.nav li.placeholder:before {
+    -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    border-color: red transparent -moz-use-text-color;
+    border-image: none;
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-style: solid solid none;
+    border-width: 5px 5px medium;
+    content: "";
+    height: 0;
+    margin-left: -5px;
+    position: absolute;
+    top: -6px;
+    width: 0;
+}
+ol.nav ol.dropdown-menu li.placeholder:before {
+    -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    border-color: transparent -moz-use-text-color transparent red;
+    border-image: none;
+    border-style: solid none solid solid;
+    border-width: 5px medium 5px 5px;
+    left: 10px;
+    margin-top: -5px;
+    top: 0;
+}
+	</style>
 @append
 
 @stop
